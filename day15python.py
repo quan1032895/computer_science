9c75702f5a278a4cd89783167a63a16b9d9fc943
quan1032895 <1032895@pdsb.net>
2025-03-03 10:29:16 -0500

    intial commit

---
+++ day15python.py
@@ -0,0 +1,6 @@
+# For loop example
+# While loop example
+count = 0
+while count <= 711:
+    print("While loop iteration:", count)
+    count += 1
---
+++ day5python.py
@@ -0,0 +1,10 @@
+ques1 = input(" are you spider man")
+if ques1 == "yes": 
+    print("hi spider man") 
+else:
+    ques2 = input("are you iron man")
+if ques2 == "yes":
+    print("Hi iron man")
+else:
+    ques3 = input("who you are then")
+    print("oh hi", ques3)
